31edc7c48de723fad29fd96e4c5d394df9ece0a5
LCamel1 <mamdy112889@qq.com>
2023-05-05 17:26:48 +0000

    安装redis,实现用redis队列异步处理更新slug

--- app/Observers/TopicObserver.php
+++ app/Observers/TopicObserver.php
@@ -1,10 +1,13 @@
 <?php
 
 namespace App\Observers;
+use App\Jobs\TranslateSlug;
+use Illuminate\Support\Facades\DB;
 
 
 use App\Models\Topic;
-use App\Handlers\SlugTranslateHandler;
+// use App\Handlers\SlugTranslateHandler;
+
 
 // creating, created, updating, updated, saving,
 // saved,  deleting, deleted, restoring, restored
@@ -21,10 +24,10 @@ class TopicObserver
         //生成话题摘录
         $topic->excerpt = make_excerpt($topic->content);
 
-        //如 slug字段无内容，即使用翻译器对title 进行翻译
-        if (!$topic->slug) {
-            $topic->slug = app(SlugTranslateHandler::class)->translate($topic->title);
-        }
+        //如 slug字段无内容，即使用翻译器对title 进行翻译(没用队列之前使用这种同步保存slug信息)
+        // if (!$topic->slug) {
+        //     $topic->slug = app(SlugTranslateHandler::class)->translate($topic->title);
+        // }
     }
 
     /**
@@ -32,6 +35,19 @@ class TopicObserver
      */
     public function saved(Topic $topic)
     {
-        //
+        //使用队列 实现异步修改slug 从而不影响用户创建话题
+        if (!$topic->slug) {
+            // 推送任务到队列
+            dispatch(new TranslateSlug($topic));
+        }
+    }
+
+    /**
+     * 删除数据之后
+     */
+    public function deleted(Topic $topic)
+    {
+        //当用户删除话题数据之后，要把它相应的回复也删掉
+        //DB::table('replies')->where('topic_id', $topic->id)->delete();
     }
 }
