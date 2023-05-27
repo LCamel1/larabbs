b0e6bb59a1d29ed7ed8ef259d73f4b1290e8843c
LCamel1 <mamdy112889@qq.com>
2023-05-27 00:53:55 +0000

    添加中间件

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -41,7 +41,9 @@ class Kernel extends HttpKernel
 
         'api' => [
             // \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
-            \Illuminate\Routing\Middleware\ThrottleRequests::class.':api',
+            // \Illuminate\Routing\Middleware\ThrottleRequests::class.':api',
+            \App\Http\Middleware\AcceptHeader::class,
+            'throttle:api',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
         ],
     ];
